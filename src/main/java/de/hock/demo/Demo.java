be2c5e9940e103ab353ef4d2194e77ae0c116108
YannicHock <mail@yannic-hock.de>
2025-04-10 17:06:56 +0200

    Initial Commit

---
+++ src/main/java/de/hock/demo/Demo.java
@@ -0,0 +1,17 @@
+package de.hock.demo;
+
+/**
+ * The Demo class serves as the entry point for the application.
+ * It contains the main method which prints a greeting message to the console.
+ */
+public class Demo {
+
+    /**
+     * The main method is the entry point of the Java application.
+     *
+     * @param args Command-line arguments passed to the application (not used in this example).
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
